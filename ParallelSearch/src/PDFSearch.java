5b3857949e45ea11127640fadc73f6a39da398e8
who-mi7654 <who_mi801@yahoo.com>
2016-11-04 15:53:31 -0500

    Add files via upload

---
+++ ParallelSearch/src/PDFSearch.java
@@ -0,0 +1,10 @@
+
+public class PDFSearch 
+{
+    public static void main(String args[])
+    {
+        PDFSManager manager = new PDFSManager(Runtime.getRuntime().availableProcessors());
+        PDFS.setManager(manager);        
+        manager.startSearch();
+    }
+}
