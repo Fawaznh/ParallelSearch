1b4449978eea5379c67d64db4cb5861c0fc887c0
Fawaznh <fawaznx@gmail.com>
2016-11-13 20:04:08 -0600

    Update ParallelSearch.java

--- ParallelSearch/src/ParallelSearch.java
+++ ParallelSearch/src/ParallelSearch.java
@@ -7,66 +7,22 @@ import java.util.*;
  */
 public class ParallelSearch 
 {
-    
-    
-    public static List<DFS> dfsThreads; //shared
-    
 
     public static void main(String[] args)
     {
 
-        dfsThreads = new ArrayList<>();
+        //System.out.println(Runtime.getRuntime().availableProcessors());
+
         Graph g = new Graph();
-        g.setStart(5);
-        g.setGoal(767);
-        
-        generateTree(g);
-        
-        /*g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 3);
-        g.addEdge(1, 4);
-        g.addEdge(2, 5);
-        g.addEdge(2, 6);
-        g.addEdge(3, 7);
-        g.addEdge(3, 8);
+        g.generateTree(100000);      
 
-        g.addEdge(4, 5);
-        */
-        dfsThreads.add(new DFS(g,g.getStart()));
-        dfsThreads.add(new DFS(g,g.getStart()));
-        dfsThreads.add(new DFS(g,g.getStart()));
-        dfsThreads.add(new DFS(g,g.getStart()));
+        g.setStart(0);
+        g.setGoal(87655);
         
-        // only activate one thread, others will get work from it
-        dfsThreads.get(0).activate(); 
-      
         
+        new DFS(g,g.getStart());
 
     }
     
-    
-    public static void generateTree(Graph g)
-    {
-       int i = 0;
-       int max = 1000;
-       
-       while(i < max)
-       {
-           int parent = i;
-           int left= (i*2)+1;
-           int right= (i*2)+2;
-           
-           i++;
-           
-           
-           g.addEdge(parent, left);
-           g.addEdge(parent, right);
-           
-           //System.out.println("node: "+parent+"  "+left+"  "+right);
-       }
-       
-    }
-    
-    
+
 }
